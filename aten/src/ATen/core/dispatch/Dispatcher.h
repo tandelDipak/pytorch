f5f0fa87b32932f4577d55e077d015983ef3e861
Brian Hirsh <hirsheybar@fb.com>
2020-10-19 08:01:07 -0700

    first cut of adding a dangling impl test. fix #45165
    fixing dangling impl's that were found through the test:
    
    1) 8 quantized fn's. These all looked like they were manually def'd
     in aten/src/ATen/native/quantized/library.cpp, with these def's
    missing. I manually added them in.
    
    2) 5 aten functions in NamedRegistrations. None of these exist in
    native_functions.yaml so I removed them
    
    ghstack-source-id: c4ba3eaad19cf02130ffd5483bee3d2cd340cb85
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46484

--- aten/src/ATen/core/dispatch/Dispatcher.h
+++ aten/src/ATen/core/dispatch/Dispatcher.h
@@ -211,6 +211,26 @@ public:
     return dispatch_key != DispatchKey::BackendSelect;
   }
 
+  //
+  // ------------------------------------------------------------------------
+  //
+  // Assertions
+  //
+  // ------------------------------------------------------------------------
+
+  /**
+   * For testing purposes.
+   * Returns a list of all operators that were created through calls to registerImpl(),
+   * without any corresponding calls to registerDef(). This is almost certainly a bug,
+   * as the created OperatorHandle won't have any schema associated with it and users
+   * calling the op through the dispatcher won't be able to access it
+   *
+   *
+   * This function should be called after static initialization is finished, so that
+   * all registrations with the dispatcher have been finalized.
+   */
+  std::vector<OperatorHandle> findDanglingImpls() const;
+
 private:
   Dispatcher();
 
