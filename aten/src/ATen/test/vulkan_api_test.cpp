80248dfe39897a2cc5bfbbcf7cd947cf6df70a74
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-10-19 23:50:23 -0700

    Add Vulkan tensor copy.
    ghstack-source-id: d09e059d727bb654615f5a728ca608ce6f0f03a9
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/46481

--- aten/src/ATen/test/vulkan_api_test.cpp
+++ aten/src/ATen/test/vulkan_api_test.cpp
@@ -3,9 +3,39 @@
 #ifdef USE_VULKAN_API
 
 #include <ATen/native/vulkan/api/api.h>
-#include <test/cpp/jit/test_utils.h>
+
+// TODO: These functions should move to a common place.
 
 namespace {
+
+bool checkRtol(const at::Tensor& diff, const std::vector<at::Tensor>& inputs) {
+  double maxValue = 0.0;
+  for (const auto& tensor : inputs) {
+    maxValue = fmax(tensor.abs().max().item<float>(), maxValue);
+  }
+  return diff.abs().max().item<float>() < 2e-6 * maxValue;
+}
+
+bool almostEqual(const at::Tensor& a, const at::Tensor& b) {
+  return checkRtol(a - b, {a, b});
+}
+
+bool exactlyEqual(const at::Tensor& a, const at::Tensor& b) {
+  return (a - b).abs().max().item<float>() == 0.f;
+}
+
+} // namespace
+
+namespace {
+
+TEST(VulkanAPITest, copy) {
+  if (!at::native::vulkan::api::available()) {
+    return;
+  }
+
+  const auto cpu = at::rand({1, 3, 64, 64}, at::device(at::kCPU).dtype(at::kFloat));
+  ASSERT_TRUE(exactlyEqual(cpu, cpu.vulkan().cpu()));
+}
 
 TEST(VulkanAPITest, empty) {
   if (!at::native::vulkan::api::available()) {
